46c9eb75cfb9e54865f0ba93612aa1d905beb9ee
TheEnderCore <theendercore@gmail.com>
2026-03-22 01:42:39 +0200

    Frog

--- neoforge/src/main/java/com/github/alexmodguy/alexscaves/server/entity/ACFrogRegistry.java
+++ neoforge/src/main/java/com/github/alexmodguy/alexscaves/server/entity/ACFrogRegistry.java
@@ -1,15 +1,19 @@
 package com.github.alexmodguy.alexscaves.server.entity;
 
 import com.github.alexmodguy.alexscaves.AlexsCaves;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.animal.FrogVariant;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ACFrogRegistry {
 
     public static final DeferredRegister<FrogVariant> DEF_REG = DeferredRegister.create(Registries.FROG_VARIANT, AlexsCaves.MOD_ID);
 
-    public static final DeferredHolder<FrogVariant, FrogVariant> PRIMORDIAL = DEF_REG.register("primordial", () -> new FrogVariant(AlexsCaves.id("textures/entity/primordial_frog.png")));
+
+    public static final Holder<FrogVariant> PRIMORDIAL = DEF_REG.register("primordial", () -> new FrogVariant(AlexsCaves.id("textures/entity/primordial_frog.png")));
+
+    public static  void init (){
+    }
 
 }

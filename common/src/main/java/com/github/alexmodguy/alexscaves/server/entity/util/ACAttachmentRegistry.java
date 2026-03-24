e96229b428e2e000783e5af1b182ecad4b69d20f
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2026-03-24 17:24:42 -0400

    Attachment Registry cleanup

--- common/src/main/java/com/github/alexmodguy/alexscaves/server/entity/util/ACAttachmentRegistry.java
+++ common/src/main/java/com/github/alexmodguy/alexscaves/server/entity/util/ACAttachmentRegistry.java
@@ -5,6 +5,7 @@ import com.github.alexmodguy.alexscaves.platform.services.IPlatformHelper;
 import com.github.alexmodguy.alexscaves.server.misc.ACMath;
 import me.fzzyhmstrs.fzzy_config.api.ConfigApiJava;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
 import org.teamvoided.voidlib.attachments.AttachmentSupplier;
@@ -18,51 +19,37 @@ public class ACAttachmentRegistry {
     public static AttachmentSupplier<MagneticEntityData, Entity> MAGNETIC_DATA = Services.REGISTRY_HELPER.registerAttachment("magnetic_data", Entity.class,
             () -> MagneticEntityData.DEFAULT,
             builder -> builder.persistent(MagneticEntityData.CODEC)
-                    .synced(MagneticEntityData.STREAM_CODEC, (entity, player) -> {
-                        // Hacky fix for NeoForge not doing a "canSend" check before trying to sync the attachment
-                        // causing the server to crash for early magnet ticking
-                        if (Services.PLATFORM_HELPER.getPlatform() == IPlatformHelper.Platform.NEOFORGE) {
-                            // noinspection ConstantConditions
-                            if (player.connection != null) {
-                                return ConfigApiJava.network().canSend(NEO_SYNC_PAYLOAD, player);
-                            }
-                            return false;
-                        }
-                        return true;
-                    })
+                    .synced(MagneticEntityData.STREAM_CODEC, ACAttachmentRegistry::canSendNeo)
                     .copyOnDeath()
     );
 
     public static AttachmentSupplier<Optional<Vec3>, Entity> OPTIONAL_VEC = Services.REGISTRY_HELPER.registerAttachment("optional_vec", Entity.class,
             Optional::empty,
             builder -> builder.persistent(Vec3.CODEC.xmap(Optional::ofNullable, (vec) -> vec.orElse(null)))
-                    .synced(ACMath.OPTIONAL_VEC3_STREAM_CODEC, (entity, player) -> {
-                        if (Services.PLATFORM_HELPER.getPlatform() == IPlatformHelper.Platform.NEOFORGE) {
-                            if (player.connection != null) {
-                                return ConfigApiJava.network().canSend(NEO_SYNC_PAYLOAD, player);
-                            }
-                            return false;
-                        }
-                        return true;
-                    })
+                    .synced(ACMath.OPTIONAL_VEC3_STREAM_CODEC, ACAttachmentRegistry::canSendNeo)
                     .copyOnDeath()
     );
 
     public static AttachmentSupplier<GummyColors, Entity> GUMMY_COLOR = Services.REGISTRY_HELPER.registerAttachment("gummy_color", Entity.class,
             () -> GummyColors.RED,
             builder -> builder.persistent(GummyColors.CODEC)
-                    .synced(GummyColors.STREAM_CODEC, (entity, player) -> {
-                        if (Services.PLATFORM_HELPER.getPlatform() == IPlatformHelper.Platform.NEOFORGE) {
-                            if (player.connection != null) {
-                                return ConfigApiJava.network().canSend(NEO_SYNC_PAYLOAD, player);
-                            }
-                            return false;
-                        }
-                        return true;
-                    })
+                    .synced(GummyColors.STREAM_CODEC, ACAttachmentRegistry::canSendNeo)
                     .copyOnDeath()
     );
 
     public static void init() {
+    }
+
+    // Hacky fix for NeoForge not doing a "canSend" check before trying to sync the attachment
+    // causing the server to crash for early magnet ticking
+    private static boolean canSendNeo(Entity entity, ServerPlayer player) {
+        if (Services.PLATFORM_HELPER.getPlatform() == IPlatformHelper.Platform.NEOFORGE) {
+            // noinspection ConstantConditions
+            if (player.connection != null) {
+                return ConfigApiJava.network().canSend(NEO_SYNC_PAYLOAD, player);
+            }
+            return false;
+        }
+        return true;
     }
 }

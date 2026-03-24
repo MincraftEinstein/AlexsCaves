098942554509f05c83e334f44994eb0ccd8e851c
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2026-03-24 19:08:15 -0400

    Moved remaining particles to common

--- common/src/main/java/com/github/alexmodguy/alexscaves/client/particle/SundropParticle.java
+++ common/src/main/java/com/github/alexmodguy/alexscaves/client/particle/SundropParticle.java
@@ -8,8 +8,6 @@ import net.minecraft.client.particle.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.util.Mth;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 
 public class SundropParticle extends TextureSheetParticle {
 
@@ -70,7 +68,6 @@ public class SundropParticle extends TextureSheetParticle {
         return 240;
     }
 
-    @OnlyIn(Dist.CLIENT)
     public static class Factory implements ParticleProvider<SimpleParticleType> {
         private final SpriteSet spriteSet;
 

932c8befddbe49ab97057df1628ee6d1ebaac485
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2026-03-24 22:10:11 -0400

    Update NuclearFurnaceBlockEntity.java

--- common/src/main/java/com/github/alexmodguy/alexscaves/server/block/blockentity/NuclearFurnaceBlockEntity.java
+++ common/src/main/java/com/github/alexmodguy/alexscaves/server/block/blockentity/NuclearFurnaceBlockEntity.java
@@ -11,6 +11,7 @@ import com.github.alexmodguy.alexscaves.server.inventory.NuclearFurnaceMenu;
 import com.github.alexmodguy.alexscaves.server.misc.ACAdvancementTriggerRegistry;
 import com.github.alexmodguy.alexscaves.server.misc.ACTagRegistry;
 import com.github.alexmodguy.alexscaves.server.potion.ACEffectRegistry;
+import com.github.alexmodguy.alexscaves.server.recipe.ACRecipeRegistry;
 import com.google.common.collect.Lists;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
@@ -536,10 +537,9 @@ public class NuclearFurnaceBlockEntity extends BaseContainerBlockEntity implemen
     }
 
     public static RecipeType<? extends AbstractCookingRecipe> getRecipeType(){
-        // TODO fix when recipe
-      /*  if (AlexsCaves.COMMON_CONFIG.nuclearFurnaceCustomType.get()) {
+        if (AlexsCaves.COMMON_CONFIG.nuclearFurnaceCustomType.get()) {
             return ACRecipeRegistry.NUCLEAR_FURNACE_TYPE.get();
-        }*/
+        }
         return AlexsCaves.COMMON_CONFIG.nuclearFurnaceBlastingOnly.get() ? RecipeType.BLASTING : RecipeType.SMELTING;
     }
 
